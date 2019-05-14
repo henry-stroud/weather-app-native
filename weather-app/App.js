1d0f8c6e8d5466b844aac3781433a995094ccb45
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-05-14 20:49:50 +0100

    added weather components

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
+
+import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
 
 export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Gettig Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      });
+  }
+
   render() {
+    const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {isLoading ? <Text>Fetching The Weather</Text> : <Weather />}
       </View>
     );
   }
@@ -14,8 +49,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+  }
 });
---
+++ weather-app/utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '7d4dd8a6ca64e810300306facb73ccc5'
